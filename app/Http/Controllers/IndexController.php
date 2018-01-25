898e113084ad0d341d4055043fb76f9bb0ea1b9e
Islam Dudaev <islamwolf23@gmail.com>
2018-01-25 17:41:50 +0000

    Models created
    Model created:
    1. Page
    2. Portfolio
    3. Service
    4. Team

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -5,11 +5,23 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Page;
+use App\Service;
+use App\Portfolio;
+use App\Team;
+
 
 class IndexController extends Controller
 {
     public function execute(Request $request)
     {
+        $pages = Page::all();
+        $portfolio = Portfolio::get(array('name', 'filter', 'images')); // можно выбрать что нам нужно
+        $services = Service::all();
+        $team = Team::take(3)->get(); // берет три сотрудника
+
+        dd($team);
+
         return view('layouts.main');
     }
 }
