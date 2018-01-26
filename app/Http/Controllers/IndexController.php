0f46d9bc28e10642094cea22abc349a8808a108c
Islam Dudaev <islamwolf23@gmail.com>
2018-01-26 20:20:55 +0000

    IndexController@execute done
    Added pages paramethors to use in layouts.

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -20,8 +20,37 @@ class IndexController extends Controller
         $services = Service::all();
         $team = Team::take(3)->get(); // берет три сотрудника
 
+        $menu = array();
 
+        // Pages /home and /about us with section IDs home and aboutUs
+        foreach ($pages as $page){
+            $item = array('title'=>$page->name, 'alias'=>$page->alias);
+            array_push($menu, $item);
+        }
 
-        return view('front.index');
+        // page Services with section id service
+        $item = array('title'=>'Services', 'alias'=>'service');
+        array_push($menu, $item);
+
+        // page Portfolio with section id Portfolio
+        $item = array('title'=>'Portfolio', 'alias'=>'Portfolio');
+        array_push($menu, $item);
+
+        // page Team with section id team
+        $item = array('title'=>'Team', 'alias'=>'team');
+        array_push($menu, $item);
+
+        // page Contact with section id contact
+        $item = array('title'=>'Contact', 'alias'=>'contact');
+        array_push($menu, $item);
+
+        return view('front.index', array(
+            'menu'      => $menu,
+            'pages'     => $pages,
+            'team'      => $team,
+            'services'  => $services,
+            'portfolio' => $portfolio,
+        ));
+
     }
 }
