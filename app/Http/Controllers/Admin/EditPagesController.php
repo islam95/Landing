83b02a4f88b157bca81e22b75a8222f57e91b0b7
Islam Dudaev <islamwolf23@gmail.com>
2018-02-07 21:19:56 +0000

    Updating pages finished

--- app/Http/Controllers/Admin/EditPagesController.php
+++ app/Http/Controllers/Admin/EditPagesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Page;
+use Validator;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -12,7 +13,43 @@ class EditPagesController extends Controller
 {
     public function execute(Page $page, Request $request)
     {
+        if ($request->isMethod('post')){
+
+            $input = $request->except('_token');
+
+            $validator = Validator::make($input, [
+                'name'      => 'required|max:255',
+                'alias'     => 'required|max:255|unique:pages,alias,' . $input['id'],
+                'content'   => 'required',
+            ]);
+
+            if ($validator->fails()){
+                return redirect()
+                    ->route('editPages', ['page'=>$input['id']])
+                    ->withErrors($validator)
+                    ->withInput();
+            }
+
+            if ($request->hasFile('images')) {
+                $file = $request->file('images');
+                $file->move(public_path().'/assets/img', $file->getClientOriginalName());
+                $input['images'] = $file->getClientOriginalName();
+            } else {
+                $input['images'] = $input['old_images'];
+            }
+
+            unset($input['old_images']);
+
+            $page->fill($input);
+
+            if ($page->update()){
+                return redirect('admin/pages')->with('status', 'Page was successfully updated!');
+            }
+
+        }
+
         $old = $page->toArray();
+
         if (view()->exists('admin.pages_edit')){
             $data = [
                 'title' => 'Editing page - '.$old['name'],
